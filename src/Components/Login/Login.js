fd163c07eef741bffd7fd1739ffbde2c956c869c
Jakub Krasuski <kubamax4@gmail.com>
2023-03-20 21:22:03 +0100

    0.0.3

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './Login.css';
+import bg from '../../Assets/Login/Graphic Side.svg';
 
 const Login = () => {
     return (
-        <div className={'login'}>
-            <div>
-                
-            </div>
-            <div>
+        <div className={'login Regular'}>
+            <div className={'h-screen w-full'}>
 
+            </div>
+            <div className={'h-screen w-full'}>
+                <img src={bg} alt={'bg'} className={'h-screen w-full object-cover'}/>
             </div>
         </div>
     );
